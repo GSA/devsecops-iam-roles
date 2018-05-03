376a3ccc0469691d2d49fb6b28a314524cc3c3d4
Jason Miller <jmiller@red-abstract.com>
2018-05-03 16:32:01 -0500

    Adding variables for netops

--- terraform/netops/netops-iam-roles.tf
+++ terraform/netops/netops-iam-roles.tf
@@ -2,6 +2,6 @@ resource "aws_iam_role" "netops_admin_role" {
   name = "${var.netops_admin_role_name}"
 }
 
-resource "aws_iam_role" "netops_read_only_role" {
+resource "aws_iam_role" "netops_view_only_role" {
   name = "${var.netops_view_only_role_name}"
 }
---
+++ terraform/netops/netops-variables.tf
@@ -0,0 +1,9 @@
+variable "netops_admin_role_name" {
+  type    = "string"
+  default = "GRACE_NetOps_Admin_Role"
+}
+
+variable "netops_view_only_role_name" {
+  type    = "string"
+  default = "GRACE_NetOps_View_Only_Role"
+}
