259e61719f25630ed37f655087ac0cb5f802c700
Jason Miller <jmiller@red-abstract.com>
2018-05-03 16:24:59 -0500

    Adding tenant1 policies

--- terraform/tenants/tenant1/tenant-iam-roles.tf
+++ terraform/tenants/tenant1/tenant-iam-roles.tf
@@ -6,6 +6,6 @@ resource "aws_iam_role" "tenant1_power_user_role" {
   name = "${var.tenant1_power_user_role_name}"
 }
 
-resource "aws_iam_role" "tenant1_read_only_role" {
+resource "aws_iam_role" "tenant1_view_only_role" {
   name = "${var.tenant1_view_only_role_name}"
 }
