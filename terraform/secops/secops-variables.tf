94542ea84c2aee1d310510443277479ceacc1e43
Jason Miller <jmiller@red-abstract.com>
2018-05-03 15:18:23 -0500

    Added variables for secops

---
+++ terraform/secops/secops-variables.tf
@@ -0,0 +1,9 @@
+variable "secops_admin_role_name" {
+  type    = "string"
+  default = "GRACE_SecOps_Admins_Role"
+}
+
+variable "secops_view_only_role_name" {
+  type    = "string"
+  default = "GRACE_SecOps_View_Only_Role"
+}
