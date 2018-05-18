33f501d524bf692642f509b4061cf776055111c8
Aidan Feldman <aidan.feldman@gsa.gov>
2018-05-18 14:43:01 -0400

    add missing arguments on aws_iam_role

--- terraform/secops/secops-iam-roles.tf
+++ terraform/secops/secops-iam-roles.tf
@@ -1,7 +1,9 @@
 resource "aws_iam_role" "secops_admin_role" {
-  name = "${var.secops_admin_role_name}"
+  name               = "${var.secops_admin_role_name}"
+  assume_role_policy = "" # TODO
 }
 
 resource "aws_iam_role" "secops_read_only_role" {
-  name = "${var.secops_view_only_role_name}"
+  name               = "${var.secops_view_only_role_name}"
+  assume_role_policy = "" # TODO
 }
