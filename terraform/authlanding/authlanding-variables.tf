6fa45340369103a831e13d0df1846dfdddb53d80
Aidan Feldman <aidan.feldman@gsa.gov>
2018-05-18 13:07:27 -0400

    add missing variable for authlanding

--- terraform/authlanding/authlanding-variables.tf
+++ terraform/authlanding/authlanding-variables.tf
@@ -1,14 +1,15 @@
 variable "org_admin_role_name" {
-  type    = "string"
   default = "GRACE_Org_Admin_Role"
 }
 
 variable "org_finance_role_name" {
-  type    = "string"
   default = "GRACE_Org_Finance_Role"
 }
 
 variable "org_read_only_role" {
-  type    = "string"
   default = "GRACE_Org_Read_Only_Role"
+}
+
+variable "org_view_only_role_name" {
+  default = "GRACE_Org_View_Only_Role"
 }
